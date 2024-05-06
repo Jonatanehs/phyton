cdd1b77c60423fc2b5ed28a3c99297d287f74638
Leidy Johanna Bolivar Gonzalez <140461573+JohannaB95@users.noreply.github.com>
2024-05-05 19:11:12 -0500

    Update interfaz.py

---
+++ juego/interfaz.py
@@ -0,0 +1,15 @@
+from juego import nivel1, nivel2
+print("!Bienvenidos a esta nueva aventura¡")
+print("\nEn este juego, te enfrentaras a diferentes preguntas en las que pondrás a prueba tu inteligencia. "\
+    "El juego consta de 5 niveles, los cuales tienen diferentes preguntas y acertijos matemáticos y de lógica.")
+print("\nReglas:")
+print("Tendras la oportunidad de obtener 2 pistas en cada nivel", "\nPor cada pregunta con respuesta correcta"
+        " recibiras 250 de oro, ", "y 10 puntos, si contestas mal perderas 5 puntos" 
+        "\nPara poder superar un nivel deberas obtener por lo menos 3 respuestas correctas"
+        "\nSi no obtienes las respuestas correctas suficientes podrás usar el oro para reiniciar el nivel" 
+        "\nSi te quedas sin pistas y no has respondido todas las preguntas para pasar de nivel podrás usar el oro para reiniciar el nivel"
+        "\nPero si reinicias el nivel tus puntos iniciaran en 0"     
+        "\nSi no respondes correctamente a las preguntas y no tienes el oro suficiente el juego se terminara")
+print("\n¡Comencemos!")
+
+nivel1()
